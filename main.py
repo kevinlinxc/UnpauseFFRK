4f9c14c50ac9ba760cb68a2ded959a1ce2b3795a
Kevin Lin <kevinlinxc@gmail.com>
2021-07-07 00:27:50 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+import sys
+
+PERCENT_BLUE_FOR_PAUSED = 70
+
+def progress(purpose, currentcount, maxcount, skipped):
+    sys.stdout.write('\r')
+    sys.stdout.write("{}: {} of {} ({:.1f}%), {} paused frames skipped ".format(purpose,currentcount, maxcount, (100/(maxcount-1)*currentcount), skipped))
+    sys.stdout.flush()
+
+# get input video data and set up metadata for output video
+video_name = "video.mp4"
+cap = cv2.VideoCapture(video_name)
+fps = cap.get(cv2.CAP_PROP_FPS)
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+insize = (int (cap.get(cv2.CAP_PROP_FRAME_WIDTH)) ,int (cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+toppercent = 0.3
+bottompercent = 0.7
+topheight = int(toppercent * insize[1])
+bottomheight = int(bottompercent * insize[1])
+out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, insize)
+
+# read templates, convert them to black and white, and take their shapes
+pause_template_1 = cv2.imread("template1.jpg")
+pause_template_1 = cv2.cvtColor(pause_template_1, cv2.COLOR_BGR2GRAY)
+w1, h1 = pause_template_1.shape[::-1]
+pause_template_2 = cv2.imread("template2.jpg")
+pause_template_2 = cv2.cvtColor(pause_template_2, cv2.COLOR_BGR2GRAY)
+w2, h2 = pause_template_2.shape[::-1]
+
+sift = cv2.SIFT_create()
+keypoints1, descriptors1 = sift.detectAndCompute(pause_template_1, None)
+keypoints2, descriptors2 = sift.detectAndCompute(pause_template_2, None)
+counter = 0
+skippedcounter = 0
+bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+boring = 0 #300
+while cap.isOpened():
+    progress("Scanning frames", counter, frame_count, skippedcounter)
+    counter += 1
+    ret, frame = cap.read()
+    if frame is None:
+        break
+    # resize by 0.5 to speed up processing dramatically
+    rewidth = frame.shape[0]//2
+    reheight = frame.shape[1]//2
+    pixels = rewidth*reheight
+    framec = frame.copy()[topheight:bottomheight, :]
+    framec = cv2.resize(framec, (rewidth, reheight))
+    bw = cv2.cvtColor(framec, cv2.COLOR_BGR2GRAY)
+    lower_blue = np.array([110, 100, 100])
+    upper_blue = np.array([130, 255, 255])
+    hsv = cv2.cvtColor(framec, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    result = cv2.bitwise_and(framec, framec, mask=mask)
+    gray_img = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY)
+    white = [255, 255, 255]
+    count = np.count_nonzero(thresh)
+    percentblue = round(count/(rewidth*reheight)*100, 2)
+
+    print(f'Blue pixels: {count}/{pixels}, {percentblue}%')
+    # output = np.hstack([thresh, bw])
+    # cv2.namedWindow('a', cv2.WINDOW_NORMAL)
+    # cv2.imshow("a", output)
+    # cv2.resizeWindow("a", size[0]//2, size[1]//2)
+    # cv2.waitKey(0)
+    if percentblue > PERCENT_BLUE_FOR_PAUSED:
+        skippedcounter += 1
+        continue
+    else:
+        out.write(frame)
+
+cap.release()
+out.release()
+
